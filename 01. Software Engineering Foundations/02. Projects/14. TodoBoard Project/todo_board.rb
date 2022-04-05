c59d20f6660988b6e1ee28bd28197dd743d394e9
Patryk Rogala <patryk@astarium.pl>
2022-04-05 10:54:44 +0200

    Todo Board

--- 01. Software Engineering Foundations/02. Projects/14. TodoBoard Project/todo_board.rb
+++ 01. Software Engineering Foundations/02. Projects/14. TodoBoard Project/todo_board.rb
@@ -1,7 +1,7 @@
 require_relative 'list'
 
 class TodoBoard
-  def initialize(_label)
+  def initialize
     @board = {}
   end
 
@@ -10,26 +10,32 @@ class TodoBoard
     cmd, *args = gets.chomp.split(' ')
 
     case cmd
+    when 'mklist'
+      @board[args[0]] = List.new(args[0])
+    when 'ls'
+      puts @board.keys.join("\n")
+    when 'showall'
+      @board.values.each { |list| list.print }
     when 'mktodo'
-      @list.add_item(*args)
+      @board[args[0]].add_item(*args[1..-1])
     when 'up'
-      @list.up(*args.map(&:to_i))
+      @board[args[0]].up(*args[1..-1].map(&:to_i))
     when 'down'
-      @list.down(*args.map(&:to_i))
+      @board[args[0]].down(*args[1..-1].map(&:to_i))
     when 'swap'
-      @list.swap(*args.map(&:to_i))
+      @board[args[0]].swap(*args[1..-1].map(&:to_i))
     when 'sort'
-      @list.sort_by_date!
+      @board[args[0]].sort_by_date!
     when 'priority'
-      @list.print_priority
+      @board[args[0]].print_priority
     when 'print'
-      args.empty? ? @list.print : @list.print_full_item(*args.map(&:to_i))
+      args.length > 1 ? @board[args[0]].print_full_item(*args[1..-1].map(&:to_i)) : @board[args[0]].print
     when 'toggle'
-      @list.toggle_item(*args.map(&:to_i))
+      @board[args[0]].toggle_item(*args[1..-1].map(&:to_i))
     when 'rm'
-      @list.remove_item(*args.map(&:to_i))
+      @board[args[0]].remove_item(*args[1..-1].map(&:to_i))
     when 'purge'
-      @list.purge
+      @board[args[0]].purge
     when 'quit'
       return false
     else
