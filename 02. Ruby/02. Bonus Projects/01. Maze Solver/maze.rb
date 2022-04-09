5eef8416ae99f6ad98ec7caab22ea56fa9b43dbb
Patryk Rogala <rogala.patryk@pm.me>
2022-04-09 20:13:47 +0200

    Maze

--- 02. Ruby/02. Bonus Projects/01. Maze Solver/maze.rb
+++ 02. Ruby/02. Bonus Projects/01. Maze Solver/maze.rb
@@ -1,7 +1,57 @@
-file_name = ARGV[0]
+# frozen_string_literal: true
 
-maze = File.readlines(file_name).map do |line|
-  line.chomp.split('')
+# Class that represents a maze
+class Maze
+  attr_reader :start, :end, :maze
+
+  def self.from_file(file_name)
+    maze = File.readlines(file_name).map do |line|
+      line.chomp.split('')
+    end
+
+    Maze.new(maze)
+  end
+
+  def initialize(maze)
+    @maze = maze
+    @start = find_start
+    @end = find_end
+    @current = @start
+  end
+
+  def find_start
+    @maze.each_with_index do |row, row_index|
+      row.each_with_index do |cell, col_index|
+        return [row_index, col_index] if cell == 'S'
+      end
+    end
+  end
+
+  def find_end
+    @maze.each_with_index do |row, row_index|
+      row.each_with_index do |cell, col_index|
+        return [row_index, col_index] if cell == 'E'
+      end
+    end
+  end
+
+  def print
+    system('cls') || system('clear')
+
+    @maze.each do |row|
+      puts row.join
+    end
+  end
+
+  def valid_move?(pos)
+    !['*', 'S', 'X'].include?(self[pos])
+  end
+
+  def [](pos)
+    @maze[pos[0]][pos[1]]
+  end
+
+  def []=(pos, value)
+    @maze[pos[0]][pos[1]] = value
+  end
 end
-
-p maze
---
+++ 02. Ruby/02. Bonus Projects/01. Maze Solver/maze_solver.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require_relative 'maze'
+
+# class that represents maze solver to find end
+class MazeSolver
+  def initialize(maze)
+    @maze = maze
+    @start = maze.start
+    @end = maze.end
+    @current = @start
+    @path = []
+  end
+
+  def solve
+    until solved?
+      @maze.print
+      make_move
+      sleep(0.2)
+    end
+
+    puts 'You solved the maze!'
+    puts "It took you #{@path.length} moves."
+    puts 'e'
+  end
+
+  def solved?
+    @end == @current
+  end
+
+  def make_move
+    best_move = possible_moves.min_by do |move|
+      move_cost(move)
+    end
+
+    @maze[best_move] = 'X'
+    @current = best_move
+    @path << best_move
+  end
+
+  def possible_moves
+    moves = [[-1, 0], [1, 0], [0, -1], [0, 1]] # UP, DOWN, LEFT, RIGHT
+
+    moves.map do |move|
+      [@current[0] + move[0], @current[1] + move[1]]
+    end.select { |move| @maze.valid_move?(move) }
+  end
+
+  def move_cost(move)
+    (@end[0] - move[0]).abs + (@end[1] - move[1]).abs
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  file_name = ARGV[0]
+  maze = Maze.from_file(file_name)
+  solver = MazeSolver.new(maze)
+  solver.solve
+end
