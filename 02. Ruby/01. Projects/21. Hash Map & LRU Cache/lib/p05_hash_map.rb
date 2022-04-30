7a86b4d1d9001c86b908005fdf3abe06adad5586
Patryk Rogala <rogala.patryk@pm.me>
2022-04-30 20:49:49 +0200

    Phase 5: Hash Map (reprise)

--- 02. Ruby/01. Projects/21. Hash Map & LRU Cache/lib/p05_hash_map.rb
+++ 02. Ruby/01. Projects/21. Hash Map & LRU Cache/lib/p05_hash_map.rb
@@ -1,6 +1,8 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
+
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,30 +11,43 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
   def each
+    @store.flatten.each do |link|
+      link.each { |node| yield(node.key, node.val) }
+    end
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k} => #{v}"
+    end
+    "{\n#{pairs.join(",\n")}\n}"
+  end
 
-  alias_method :[], :get
-  alias_method :[]=, :set
+  alias [] get
+  alias []= set
 
   private
 
@@ -41,9 +56,14 @@ class HashMap
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+    @count = 0
+    old_store.flatten.each { |link| link.each { |node| set(node.key, node.val) } }
   end
 
   def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+    key = key.hash
+    @store[key % num_buckets]
   end
 end
