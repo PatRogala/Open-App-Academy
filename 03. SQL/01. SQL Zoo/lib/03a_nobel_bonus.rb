1ec47093582027bfc8d52783741f385a4ba983d0
Patryk Rogala <rogala.patryk@pm.me>
2022-05-02 11:35:16 +0200

    SQL Zoo

--- 03. SQL/01. SQL Zoo/lib/03a_nobel_bonus.rb
+++ 03. SQL/01. SQL Zoo/lib/03a_nobel_bonus.rb
@@ -6,10 +6,23 @@
 #  subject     :string
 #  winner      :string
 
-require_relative './sqlzoo.rb'
+require_relative './sqlzoo'
 
 def physics_no_chemistry
   # In which years was the Physics prize awarded, but no Chemistry prize?
   execute(<<-SQL)
+    SELECT DISTINCT
+      yr
+    FROM
+      nobels
+    WHERE
+      subject = 'Physics' AND yr NOT IN (
+        SELECT
+          yr
+        FROM
+          nobels
+        WHERE
+          subject = 'Chemistry'
+      )
   SQL
 end
--- 03. SQL/01. SQL Zoo/lib/04a_countries_bonus.rb
+++ 03. SQL/01. SQL Zoo/lib/04a_countries_bonus.rb
@@ -8,12 +8,25 @@
 #  population  :integer
 #  gdp         :integer
 
-require_relative './sqlzoo.rb'
+require_relative './sqlzoo'
 
 def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+          MAX(gdp)
+        FROM
+          countries
+        WHERE
+          continent = 'Europe'
+      )
   SQL
 end
 
@@ -21,6 +34,19 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      continent, name, area
+    FROM
+      countries as c
+    WHERE
+      area = (
+        SELECT
+          MAX(area)
+        FROM
+          countries
+        WHERE
+          continent = c.continent
+      )
   SQL
 end
 
@@ -28,5 +54,17 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      name, continent
+    FROM
+      countries as c
+    WHERE
+      population > (
+        SELECT
+          MAX(population)
+        FROM
+          countries
+        WHERE
+          continent = c.continent AND name != c.name) * 3
   SQL
 end
