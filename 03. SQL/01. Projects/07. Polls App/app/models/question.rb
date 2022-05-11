7c467d5abaaa1a017667f8c47bca1940f1485623
Patryk Rogala <rogala.patryk@pm.me>
2022-05-11 17:43:08 +0200

    Polls

--- 03. SQL/01. Projects/07. Polls App/app/models/question.rb
+++ 03. SQL/01. Projects/07. Polls App/app/models/question.rb
@@ -4,4 +4,19 @@ class Question < ApplicationRecord
   belongs_to :poll
 
   has_many :answer_choices, dependent: :destroy
+  has_many :responses, through: :answer_choices, source: :responses
+
+  def results
+    acs = AnswerChoice.find_by_sql(<<-SQL, id)
+      SELECT answer_choices.text, COUNT(responses.id) AS num_responses
+      FROM answer_choices
+      LEFT OUTER JOIN responses ON answer_choices.id = responses.answer_choice_id
+      WHERE answer_choices.question_id = ?
+      GROUP BY answer_choices.id
+    SQL
+
+    acs.each_with_object({}) do |ac, results|
+      results[ac.text] = ac.num_responses
+    end
+  end
 end
--- 03. SQL/01. Projects/07. Polls App/app/models/response.rb
+++ 03. SQL/01. Projects/07. Polls App/app/models/response.rb
@@ -1,4 +1,25 @@
 class Response < ApplicationRecord
   belongs_to :answer_choice
   belongs_to :respondent, class_name: 'User', foreign_key: 'user_id'
+
+  has_one :question, through: :answer_choice, source: :question
+
+  validate :not_duplicate_response
+  validate :cant_respond_to_own_poll
+
+  def sibling_responses
+    question.responses.where.not(id:)
+  end
+
+  def respondent_already_answered?
+    sibling_responses.exists?(user_id:)
+  end
+
+  def not_duplicate_response
+    errors[:base] << 'Respondent has already answered this question' if respondent_already_answered?
+  end
+
+  def cant_respond_to_own_poll
+    errors[:base] << 'Respondent cant answer to his own poll' if question.poll.author_id == user_id
+  end
 end
