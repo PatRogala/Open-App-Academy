2371ccb197f7b6d22fbb99a37b81dcdb9a09fc1a
Patryk Rogala <rogala.patryk@pm.me>
2022-05-15 20:41:38 +0200

    Phase I: SQLObject: Overview

--- 03. SQL/01. Projects/08. Build Your Own ActiveRecord/lib/01_sql_object.rb
+++ 03. SQL/01. Projects/08. Build Your Own ActiveRecord/lib/01_sql_object.rb
@@ -5,53 +5,104 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    @query ||= DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    @query.first.map(&:to_sym)
   end
 
   def self.finalize!
+    columns.each do |column|
+      define_method(column) { attributes[column] }
+      define_method("#{column}=") { |value| attributes[column] = value }
+    end
   end
 
-  def self.table_name=(table_name)
-    # ...
+  class << self
+    attr_writer :table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || name.underscore.pluralize
   end
 
   def self.all
-    # ...
+    query = DBConnection.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(query)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |result| new(result) }
   end
 
   def self.find(id)
-    # ...
+    query = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        id = ?
+    SQL
+
+    parse_all(query).first
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      raise "unknown attribute '#{attr_name}'" unless self.class.columns.include?(attr_name)
+
+      send("#{attr_name}=", value)
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |column| send(column) }
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.join(', ')
+    question_marks = (['?'] * self.class.columns.length).join(', ')
+
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    columns = self.class.columns.map { |column| "#{column} = ?" }.join(', ')
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{columns}
+      WHERE
+        id = ?
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
