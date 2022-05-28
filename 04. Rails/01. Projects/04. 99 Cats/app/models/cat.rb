9f4f252ed2949ba382d4e013b9da8a62cbba0baf
Patryk Rogala <rogala.patryk@pm.me>
2022-05-28 23:58:55 +0200

    Cat Model

---
+++ 04. Rails/01. Projects/04. 99 Cats/app/models/cat.rb
@@ -0,0 +1,16 @@
+require 'action_view'
+
+# Represents a cat
+class Cat < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
+  CAT_COLORS = %w[black white orange brown].freeze
+
+  validates :birth_date, :name, :color, :sex, :description, presence: true
+  validates :color, inclusion: { in: CAT_COLORS, message: 'is not a valid color' }
+  validates :sex, inclusion: { in: %w[F M], message: 'is not a valid option' }
+
+  def age
+    time_ago_in_words(birth_date)
+  end
+end
