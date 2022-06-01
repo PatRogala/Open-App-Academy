70a41dd5df235bcc439e5d11c1e65c082822153c
Patryk Rogala <rogala.patryk@pm.me>
2022-06-01 14:35:57 +0200

    Rails Auth

--- 04. Rails/02. Homeworks/01. Rails Auth/app/models/user.rb
+++ 04. Rails/02. Homeworks/01. Rails Auth/app/models/user.rb
@@ -1,4 +1,38 @@
 class User < ApplicationRecord
-  validates :username, :session_token, :password_digest, presence: true
+  before_validation :ensure_session_token
+
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
   validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  def self.find_by_credentials(username, _password)
+    user = User.find_by(username:)
+    return nil if user.nil?
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save!
+    session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
 end
