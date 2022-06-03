33fa60bb45410d2d66acaac05bb80e304e549e2e
Patryk Rogala <rogala.patryk@pm.me>
2022-06-03 07:15:34 +0200

    Add a User model

---
+++ 04. Rails/01. Projects/04. 99 Cats/app/models/user.rb
@@ -0,0 +1,32 @@
+class User
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+
+  after_initialize :ensure_session_token
+
+  def self.reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    save!
+    session_token
+  end
+
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(username: user_name)
+    return nil unless user
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def ensure_session_token!
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
