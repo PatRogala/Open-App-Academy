85a720a7bb5144b1891c19df5c3392dd3f06c666
Patryk Rogala <rogala.patryk@pm.me>
2022-07-07 09:23:41 +0200

    Phase Vb: Write the Router

--- 04. Rails/01. Projects/09. Rails Lite/lib/router.rb
+++ 04. Rails/01. Projects/09. Rails Lite/lib/router.rb
@@ -21,10 +21,14 @@ end
 class Router
   attr_reader :routes
 
-  def initialize; end
+  def initialize
+    @routes = []
+  end
 
   # simply adds a new route to the list of routes
-  def add_route(pattern, method, controller_class, action_name); end
+  def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
+  end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
@@ -33,10 +37,15 @@ class Router
   # make each of these methods that
   # when called add route
   %i[get post put delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
-  def match(req); end
+  def match(req)
+    routes.find { |route| route.matches?(req) }
+  end
 
   # either throw 404 or call run on a matched route
   def run(req, res); end
