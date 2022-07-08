a7aabbb4eaa37071da65fa5bd991500a4b94a138
Patryk Rogala <rogala.patryk@pm.me>
2022-07-08 21:22:23 +0200

    Phase II: Constructors, Prototypes, and this

--- 05. JavaScript/01. Homeworks/01. Intro JavaScript Problems/part_2.js
+++ 05. JavaScript/01. Homeworks/01. Intro JavaScript Problems/part_2.js
@@ -4,3 +4,26 @@ function titleize(arr, callback) {
   });
   callback(newArr);
 }
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function() {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRR!!!!!!!'`);
+}
+
+Elephant.prototype.grow = function() {
+  this.height += 12;
+}
+
+Elephant.prototype.addTrick = function(trick) {
+  this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function() {
+  var trick = this.tricks[Math.floor(Math.random() * this.tricks.length)];
+  console.log(`${this.name} is ${trick}!`);
+}
