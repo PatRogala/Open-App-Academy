71e30590a5ac02ff5cdcafb12d9b6b58cb8b2a52
Patryk Rogala <rogala.patryk@pm.me>
2022-07-10 19:33:32 +0200

    Phase II: Draw a Rectangle

--- 05. JavaScript/01. Homeworks/03. Intro to Canvas/practice.js
+++ 05. JavaScript/01. Homeworks/03. Intro to Canvas/practice.js
@@ -3,4 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
   canvas.width = 500;
   canvas.height = 500;
   const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 250, 250);
 });
