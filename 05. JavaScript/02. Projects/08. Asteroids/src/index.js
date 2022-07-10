2245c23ac957380463458ef2b4fad1cee5afd7cc
Patryk Rogala <rogala.patryk@pm.me>
2022-07-10 21:09:46 +0200

    MovingObject

---
+++ 05. JavaScript/02. Projects/08. Asteroids/src/index.js
@@ -0,0 +1,8 @@
+const MovingObject = require('./moving_object');
+
+window.MovingObject = MovingObject;
+
+window.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById("game-canvas");
+  const ctx = canvas.getContext("2d");
+});
