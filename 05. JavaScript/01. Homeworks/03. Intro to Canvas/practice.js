909fdb97e36b1dadd1a2674593b7e3ce4f9b73ee
Patryk Rogala <rogala.patryk@pm.me>
2022-07-10 19:31:27 +0200

    Phase I: Setting up your Canvas Element

---
+++ 05. JavaScript/01. Homeworks/03. Intro to Canvas/practice.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById("mycanvas");
+  canvas.width = 500;
+  canvas.height = 500;
+  const ctx = canvas.getContext("2d");
+});
