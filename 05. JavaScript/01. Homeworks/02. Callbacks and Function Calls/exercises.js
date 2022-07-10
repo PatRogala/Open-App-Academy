ca56431826d7fe65b518db7d0d8136dd0092b36e
Patryk Rogala <rogala.patryk@pm.me>
2022-07-10 11:05:32 +0200

    Some tea? Some biscuits?

--- 05. JavaScript/01. Homeworks/02. Callbacks and Function Calls/exercises.js
+++ 05. JavaScript/01. Homeworks/02. Callbacks and Function Calls/exercises.js
@@ -5,3 +5,18 @@ window.setTimeout(() => alert('HAMMERTIME'), 5000);
 function hammerTime(time) {
   window.setTimeout(() => alert(`${time} is hammertime!`), time);
 }
+
+// Some tea? Some biscuits?
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Would you like a tea? ', (firstAns) => {
+  console.log(`You said ${firstAns}`);
+  rl.question('Would you like some biscuits? ', (secondAns) => {
+    console.log(`So you ${firstAns} want tea and you ${secondAns} want coffee.`);
+    rl.close();
+  });
+});
