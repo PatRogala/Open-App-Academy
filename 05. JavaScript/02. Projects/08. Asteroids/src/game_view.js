e192b8203f926d86384bcfbc577ecb1375aea8d1
Patryk Rogala <rogala.patryk@pm.me>
2022-07-11 20:59:27 +0200

    Phase 3b: Moving the ship

--- 05. JavaScript/02. Projects/08. Asteroids/src/game_view.js
+++ 05. JavaScript/02. Projects/08. Asteroids/src/game_view.js
@@ -4,10 +4,34 @@ function GameView(game, ctx) {
 }
 
 GameView.prototype.start = function() {
+  this.bindKeyHandlers();
   setInterval(() => {
     this.game.step();
     this.game.draw(this.ctx);
   }, 20);
+}
+
+GameView.prototype.bindKeyHandlers = function() {
+  key('space', () => {
+    this.game.ship.fireBullet();
+  }
+  );
+  key('w', () => {
+    this.game.ship.power([0, -1]);
+  }
+  );
+  key('a', () => {
+    this.game.ship.power([-1, 0]);
+  }
+  );
+  key('s', () => {
+    this.game.ship.power([0, 1]);
+  }
+  );
+  key('d', () => {
+    this.game.ship.power([1, 0]);
+  }
+  );
 }
 
 module.exports = GameView;
