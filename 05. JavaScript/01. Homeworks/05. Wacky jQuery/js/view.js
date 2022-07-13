de25465a27a2233b71eface155135189007c7766
Patryk Rogala <rogala.patryk@pm.me>
2022-07-13 23:28:25 +0200

    Exercises

--- 05. JavaScript/01. Homeworks/05. Wacky jQuery/js/view.js
+++ 05. JavaScript/01. Homeworks/05. Wacky jQuery/js/view.js
@@ -22,28 +22,28 @@ View.prototype.exercise1 = function () {
   //Challenge: Give every square the class 'orange'
   //Result: Every square should turn orange (we already have a CSS rule)
 
-  //your code here!
+  $(".square").addClass("orange");
 };
 
 View.prototype.exercise2 = function () {
   //Challenge: Remove every square
   //Result: Every square vanishes
 
-  //your code here!
+  $(".square").remove();
 };
 
 View.prototype.exercise3 = function () {
   //Challenge: Add an <h1> with the text 'i love jquery' under the grid.
   //Result: An <h1> with the text 'i love jquery' appears under the grid.
 
-  //your code here!
+  $("#easel").append($("<h1>").text("i love jquery"));
 };
 
 View.prototype.exercise4 = function () {
   //Challenge: Write your first name in every other square.
   //Result: Your name appears in every other square.
 
-  //your code here!
+  $(".square:nth-child(2n)").text("Patryk");
 };
 
 View.prototype.exercise5 = function () {
@@ -54,7 +54,9 @@ View.prototype.exercise5 = function () {
   //hint: checkout the addRow function at the bottom of the file: we set the
   //  'data-pos' of every square
 
-  //your code here!
+  $(".square").on("click", (e) => {
+    alert($(e.currentTarget).data("pos"));
+  });
 };
 
 View.prototype.exercise6 = function () {
@@ -65,6 +67,9 @@ View.prototype.exercise6 = function () {
   //hint: use window._randomColorString() (defined at top) to get a random color!
 
   //your code here!
+  $(".square").each((idx, square) => {
+    $(square).css("background-color", _randomColorString());
+  });
 };
 
 View.prototype.exercise7 = function(){
@@ -75,6 +80,9 @@ View.prototype.exercise7 = function(){
   //rainbow.
 
   //your code here!
+  $(".square").on("mouseenter", (e) => {
+    console.log($(e.currentTarget).css("background-color"));
+  });
 };
 
 
