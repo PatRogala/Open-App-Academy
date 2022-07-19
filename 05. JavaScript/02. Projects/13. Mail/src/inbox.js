388af86b59274251ba543e63fb3d4d6df093bcde
Patryk Rogala <rogala.patryk@pm.me>
2022-07-19 22:14:56 +0200

    MessageStore

--- 05. JavaScript/02. Projects/13. Mail/src/inbox.js
+++ 05. JavaScript/02. Projects/13. Mail/src/inbox.js
@@ -1,6 +1,9 @@
+const MessageStore = require('./message_store.js');
+
 const Inbox = {
   render: () => {
     let container = document.createElement('ul');
+    const messages = MessageStore.getInboxMessages();
     container.className = 'messages';
     container.innerHTML = 'An Inbox Message';
     return container;
