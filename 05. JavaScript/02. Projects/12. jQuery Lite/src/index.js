197ae638162ccdda6eed1d0d8a27d60425a4457b
Patryk Rogala <rogala.patryk@pm.me>
2022-07-19 14:11:47 +0200

    Phase 3: Document Ready

--- 05. JavaScript/02. Projects/12. jQuery Lite/src/index.js
+++ 05. JavaScript/02. Projects/12. jQuery Lite/src/index.js
@@ -1,12 +1,14 @@
 const DOMNodeCollection = require('./dom_node_collection.js');
 
-document.addEventListener('DOMContentLoaded', function() {
-  window.$l = function(selector) {
-    if (selector instanceof HTMLElement) {
-      return new DOMNodeCollection([selector]);
-    }
-
-    const elements = document.querySelectorAll(selector);
-    return new DOMNodeCollection(Array.from(elements));
+window.$l = function(selector) {
+  if (selector instanceof HTMLElement) {
+    return new DOMNodeCollection([selector]);
   }
-});
+
+  if (selector instanceof Function) {
+    document.addEventListener('DOMContentLoaded', selector);
+  }
+
+  const nodes = document.querySelectorAll(selector);
+  return new DOMNodeCollection(Array.from(nodes));
+}
