f0c518fe73333ec278622d48876f042b1f53cfc6
Patryk Rogala <rogala.patryk@pm.me>
2022-07-23 22:44:19 +0200

    Skeleton

---
+++ 06. React/02. Projects/01. Widgets/widgets.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <Root />, root);
+});
