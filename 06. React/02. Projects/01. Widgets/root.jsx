e3fe63801f335051e15a4ff8d9c7cf579dab76dd
Patryk Rogala <rogala.patryk@pm.me>
2022-07-23 22:54:15 +0200

    Phase 1: Setup

---
+++ 06. React/02. Projects/01. Widgets/root.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Root = () =>  (
+  <div>
+    <h1>Root</h1>
+    <p>This is the root of the app</p>
+  </div>
+);
+
+export default Root;
--- 06. React/02. Projects/01. Widgets/widgets.jsx
+++ 06. React/02. Projects/01. Widgets/widgets.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Root from './root';
 
 document.addEventListener('DOMContentLoaded', function() {
   const root = document.getElementById('root');
