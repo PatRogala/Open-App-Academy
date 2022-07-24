3fe532709f456ec8e5cf9655e910b9408d6b8ad0
Patryk Rogala <rogala.patryk@pm.me>
2022-07-24 13:23:29 +0200

    Phase 1: Game

--- 06. React/02. Projects/02. Minesweeper/components/game.jsx
+++ 06. React/02. Projects/02. Minesweeper/components/game.jsx
@@ -3,5 +3,21 @@ import * as Minesweeper from '../react_minesweeper.jsx';
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      board: new Minesweeper.Board(9, 10),
+    };
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame() {
+    // TODO: update the game state
+  }
+
+  render() {
+    return (
+      <Board board={this.state.board} updateGame={this.updateGame} />
+    );
   }
 }
+
+export default Game;
