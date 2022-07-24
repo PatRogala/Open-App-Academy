2774c00a2f13fd5f72220d62c2b581865278f9b5
Patryk Rogala <rogala.patryk@pm.me>
2022-07-24 10:28:13 +0200

    Phase 3: Tabs Widget

---
+++ 06. React/02. Projects/01. Widgets/frontend/tabs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 0
+    };
+  }
+
+  render() {
+    const {selectedTab} = this.state;
+    const {tabs} = this.props;
+    return (
+      <div>
+        <h1>Tabs</h1>
+        <div class="tabs">
+          <div class="tab-header">
+            <ul>
+              {tabs.map((tab, index) => (
+                <li key={index} className={index === selectedTab ? 'selected' : ''} onClick={() => this.setState({selectedTab: index})}>
+                  {tab.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <article className="tab-content">
+            <p>{tabs[selectedTab].content}</p>
+          </article>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tabs;
--- 06. React/02. Projects/01. Widgets/root.jsx
+++ 06. React/02. Projects/01. Widgets/root.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Clock from "./frontend/clock.jsx";
+import Tabs from "./frontend/tabs.jsx";
 
 const Root = () =>  (
   <div>
     <Clock />
+    <Tabs tabs={[
+      {title: "Tab 1", content: "This is the first tab"},
+      {title: "Tab 2", content: "This is the second tab"},
+      {title: "Tab 3", content: "This is the third tab"}
+    ]} />
   </div>
 );
 
