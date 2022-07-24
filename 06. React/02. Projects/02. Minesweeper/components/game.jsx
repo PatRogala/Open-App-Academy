0aa76538ce01fb7e6afb18b2e2a5f2f30bd7e694
Patryk Rogala <rogala.patryk@pm.me>
2022-07-24 19:10:49 +0200

    Phase 5: Modal

--- 06. React/02. Projects/02. Minesweeper/components/game.jsx
+++ 06. React/02. Projects/02. Minesweeper/components/game.jsx
@@ -9,15 +9,26 @@ class Game extends React.Component {
       board: new Minesweeper.Board(9, 10)
     };
     this.updateGame = this.updateGame.bind(this);
+    this.resetGame = this.resetGame.bind(this);
   }
 
-  updateGame() {
-    // TODO: update the game state
+  updateGame(tile, altKeyed) {
+    altKeyed ? tile.toggleFlag() : tile.explore();
+    this.setState({ board: this.state.board })
+  }
+
+  resetGame(e) {
+    e.preventDefault();
+    this.setState({ board: new Minesweeper.Board(9, 10) });
   }
 
   render() {
+    const {board} = this.state;
     return (
-      <Board board={this.state.board} updateGame={this.updateGame} />
+      <div>
+        <div className={`modal-screen ${board.lost() || board.won() ? 'visible' : ''}`}><div class="modal-content"><p>Its over!</p><button onClick={this.resetGame}>Play Again</button></div></div>
+        <Board board={this.state.board} updateGame={this.updateGame} />
+      </div>
     );
   }
 }
